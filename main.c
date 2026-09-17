7cee90b3fb5b4b6604d9f4680e281ef038e3b0bf
Jose Rian <joserian706@gmail.com>
2026-09-16 21:26:50 -0400

    Adiciona produtos ao estoque

--- main.c
+++ main.c
@@ -12,24 +12,29 @@ void exibir_menu(void) {
 
 void listar_produtos(Produto lista[], int total) {
     printf("\n--- Produtos Cadastrados ---\n");
+
     for (int i = 0; i < total; i++) {
-        // BUG: esqueceram de imprimir o ID e a quebra de linha está inadequada
-        printf("Nome: %s | Preco: R$ %.2f | Qtd: %d", lista[i].nome, lista[i].preco, lista[i].quantidade);
+        printf("ID: %d | Nome: %s | Preco: R$ %.2f | Qtd: %d\n",
+               lista[i].id,
+               lista[i].nome,
+               lista[i].preco,
+               lista[i].quantidade);
     }
 }
 
 float calcular_total(Produto lista[], int total) {
     float soma = 0.0;
+
     for (int i = 0; i < total; i++) {
-        // BUG: calculo multiplicando errado e nao aplica taxa
-        soma += lista[i].preco;
+        soma += lista[i].preco * lista[i].quantidade;
     }
+
     return soma;
 }
 
 int main(void) {
     Produto estoque[MAX_ITENS];
-    int total_produtos = 2;
+    int total_produtos = 4;
 
     estoque[0].id = 1;
     strcpy(estoque[0].nome, "Caderno");
@@ -41,9 +46,21 @@ int main(void) {
     estoque[1].preco = 3.00;
     estoque[1].quantidade = 50;
 
+    estoque[2].id = 3;
+    strcpy(estoque[2].nome, "Lapis");
+    estoque[2].preco = 1.50;
+    estoque[2].quantidade = 50;
+
+    estoque[3].id = 4;
+    strcpy(estoque[3].nome, "Borracha");
+    estoque[3].preco = 0.75;
+    estoque[3].quantidade = 50;
+
     int opcao = -1;
+
     while (opcao != 0) {
         exibir_menu();
+
         if (scanf("%d", &opcao) != 1) {
             break;
         }
@@ -52,12 +69,16 @@ int main(void) {
             case 1:
                 listar_produtos(estoque, total_produtos);
                 break;
+
             case 2:
-                printf("\nTotal em estoque: R$ %.2f\n", calcular_total(estoque, total_produtos));
+                printf("\nTotal em estoque: R$ %.2f\n",
+                       calcular_total(estoque, total_produtos));
                 break;
+
             case 0:
                 printf("\nEncerrando o programa...\n");
                 break;
+
             default:
                 printf("\nOpcao invalida!\n");
                 break;
